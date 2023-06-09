211edcc9cb4845611dac1555a60dc59884f48141
Julien Danjou <julien@danjou.info>
2023-06-09 11:55:59 +0200

    docs: add Jenkins integration (#1358)

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -11,3 +11,19 @@ export function Screenshot(props) {
     <img style={imgStyle} {...props} />
   );
 }
+
+
+export function IntegrationLogo(props) {
+
+  const imgStyle = {
+    float: 'right',
+    marginLeft: '1em',
+    marginBottom: '1em',
+    maxWidth: '256px',
+    maxHeight: '256px',
+  };
+
+  return (
+    <img style={imgStyle} {...props} />
+  );
+}
