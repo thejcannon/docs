1545167f4119511558f276126006406ed25ee90e
AlexandreGaubert <40125772+AlexandreGaubert@users.noreply.github.com>
2023-06-13 11:11:57 +0200

    feat: Add plan feature tags (#1388)
    <img width="860" alt="image" src="https://github.com/Mergifyio/docs/assets/40125772/8feeb4d3-77b9-418c-8afd-6e7102f192eb">

---
+++ src/components/PlanTags.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Flex, Link, Tag, TagLabel, TagLeftIcon} from '@chakra-ui/react'
+import {FaCrown} from 'react-icons/fa'
+import {MdOutlineWorkspacePremium} from 'react-icons/md'
+import {BsCode} from 'react-icons/bs'
+
+interface Props {
+  advanced: boolean;
+  premium: boolean;
+  openSource: boolean;
+}
+
+/**
+ * Renders tags for each plan advanced, premium and opensource. Meaning that a feature is available for
+ * one or more of these plans.
+ * 
+ * *Must be placed beside a title to avoid big margins*
+ * @example `##Awesome feature <PlanTags advanced premium />`
+ * @returns 
+ */
+export default function PlanTags({advanced, premium, openSource}: Props) {
+  return (
+    <Flex alignItems={'flex-start'} gap='2' marginTop={2}>
+      {advanced && (
+        <Link href='https://mergify.com/pricing' target={'_blank'} rel='noopenner'>
+          <Tag size={'md'} colorScheme='orange'>
+            <TagLeftIcon as={FaCrown} />
+            <TagLabel>Advanced Plan Feature</TagLabel>
+          </Tag>
+        </Link>
+      )}
+      {premium && (
+        <Link href='https://mergify.com/pricing' target={'_blank'} rel='noopenner'>
+          <Tag size={'md'} colorScheme='blue'>
+            <TagLeftIcon as={MdOutlineWorkspacePremium} />
+            <TagLabel>Premium Plan Feature</TagLabel>
+          </Tag>  
+        </Link>
+      )}
+      {openSource && (
+        <Tag size={'md'} colorScheme='green'>
+          <TagLeftIcon as={BsCode} />
+          <TagLabel>Open Source Feature</TagLabel>
+        </Tag>
+      )}
+    </Flex>
+  )
+}
