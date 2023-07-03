74aa1a6a76408d939b070f591fd0e1282fc05694
AlexandreGaubert <40125772+AlexandreGaubert@users.noreply.github.com>
2023-07-03 10:17:02 +0200

    fix(api): Bad retrieving of openapi.json (#1552)
    There is a `apiDescriptionUrl` which is meant for specifying the url of the `openapi.json` in the stoplight component. But it is not working. So instead I am manually fetching the json and passing it to the component.

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -1,12 +1,35 @@
 import { API } from '@stoplight/elements';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '@stoplight/elements/styles.min.css';
 
+async function getOpenapiDocument(): Promise<string> {
+  try {
+    const response = await window.fetch('https://api.mergify.com/v1/openapi.json');
+
+    if (response.ok) {
+      return await response.json();
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to fetch openapi.json.');
+  }
+
+  return '';
+}
+
 export default function APISpecifications() {
+  const [openapiDocument, setOpenapiDocument] = useState<string | null>(null);
+
+  useEffect(() => {
+    getOpenapiDocument().then((doc) => {
+      setOpenapiDocument(doc);
+    });
+  }, []);
+
   return (
     <div>
-      <API apiDescriptionDocument="https://api.mergify.com/v1/openapi.json" basePath="/api" router={typeof window === 'undefined' ? 'memory' : 'history'} />
+      {!!openapiDocument && <API apiDescriptionDocument={openapiDocument} basePath="/api" router={typeof window === 'undefined' ? 'memory' : 'history'} />}
     </div>
   );
 }
