5cf6867ee7695bf0a07c27fcfed7a5d6864303df
Mehdi ABAAKOUK <sileht@sileht.net>
2023-10-30 09:01:30 +0100

    fix: use static openapi.json (#2669)
    The file was temporary add to api.mergify.com. This file is live built
    and has some bugs.
    
    This change uses the static and valid file generated by engine
    repository.
    
    This file is auto updated by a GitHub action
    
    Fixes MRGFY-2833

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -1,35 +1,14 @@
 import { API } from '@stoplight/elements';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import '@stoplight/elements/styles.min.css';
 
-async function getOpenapiDocument(): Promise<string> {
-  try {
-    const response = await window.fetch('https://api.mergify.com/v1/openapi.json');
-
-    if (response.ok) {
-      return await response.json();
-    }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Failed to fetch openapi.json.');
-  }
-
-  return '';
-}
+import ApiSchemas from './api-schemas.json';
 
 export default function APISpecifications() {
-  const [openapiDocument, setOpenapiDocument] = useState<string | null>(null);
-
-  useEffect(() => {
-    getOpenapiDocument().then((doc) => {
-      setOpenapiDocument(doc);
-    });
-  }, []);
-
   return (
     <div>
-      {!!openapiDocument && <API apiDescriptionDocument={openapiDocument} basePath="/api" router={typeof window === 'undefined' ? 'memory' : 'history'} />}
+      <API apiDescriptionDocument={ApiSchemas} basePath="/api" router={typeof window === 'undefined' ? 'memory' : 'history'} />
     </div>
   );
 }
