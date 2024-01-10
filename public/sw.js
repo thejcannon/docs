965238911ec66b4d47e3577f5ba4601748cf04fa
Mehdi ABAAKOUK <sileht@sileht.net>
2024-01-10 14:56:08 +0100

    fix: readd the Gatsby service worker (#3149)
    This readd the service worker file add by Gatsby.
    
    But replace the code to unregister it.

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function () {
+    self.skipWaiting();
+});
+self.addEventListener('activate', function () {
+    console.log(`Unregistering service worker`)
+    self.registration.unregister()
+        .then(function () {
+            return self.clients.matchAll();
+        })
+        .then(function (clients) {
+            clients.forEach(client => {
+                console.log(`Navigating ${client.url}`)
+                client.navigate(client.url)
+            })
+        });
+});
